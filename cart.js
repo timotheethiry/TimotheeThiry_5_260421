ce6f25916f2d81578148c2ba07de8edf158f1d92
Timothée Thiry <timothee.thiry@gmail.com>
2021-06-18 18:17:26 +0200

    refactor cart.js

--- cart.js
+++ cart.js
@@ -1,79 +1,70 @@
-// cart functions
+/* -- cart functions -- */
 
-let cart =[]; // warning variable globale
+const localStorageCartKey = "cart";
 
-
-// get cart
+/* get cart -- load the cart from the storage */ 
 
 function getCart() {
-  cart = JSON.parse(localStorage.getItem("cart"));
+  return JSON.parse(localStorage.getItem(localStorageCartKey)); 
 }
 
 
-// store cart
+// find the name of every item in the cart and display it
 
-function storeCart() {
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-
-
-
-// -- cart functions
-
-
-// find names of items in the cart
-
-function findName(obj) {
-  teddy = obj.find((item) => item._id == cart[i]);
+function findName(obj, i) {
+  const cart = getCart();
+  const teddy = obj.find((item) => item._id == cart[i]);
   return teddy.name;
 }
 
-let cartNames;
-
 function findAllNames(arg) {
-  cartNames = [];
-  for (i = 0; i < cart.length; i++) {
-    findName(arg);
-    cartNames.push(teddy.name);
+  const cart = getCart();
+  let cartNames = [];
+  for (let i = 0, j = cart.length; i < j; i++) {
+    const teddyName = findName(arg, i);
+    cartNames.push(teddyName);
   }
   return cartNames;
 }
 
-
-// display cart items
-
-function displayCart() {
+function displayCart(arg) {
+  const cart = getCart();
+  const cartNames = findAllNames(arg);
   if (!cart.length) {
     document.getElementById("cart-items").textContent = "is empty";
   } else if (cart.length > 1) {
-    document.getElementById("cart-items").textContent = "has" + " " + cartNames.length + " " + "items" + "," + " " + cartNames.join(", ");
+    document.getElementById("cart-items").textContent = "has" + " " + 
+    cartNames.length + " " + "items" + "," + " " + cartNames.join(", ");
   } else {
-    document.getElementById("cart-items").textContent = "has" + " " + cartNames.length + " " + "item" + "," + " " + cartNames.join(", ");
+    document.getElementById("cart-items").textContent = "has" + " " + 
+    cartNames.length + " " + "item" + "," + " " + cartNames.join(", ");
   }
 }
 
 
-// find price, find total price, display total
+/* find the price of every item in the cart, 
+calculate and display the total price*/
 
-function findPrice(obj) {
-  teddy = obj.find((item) => item._id == cart[i]);
+function findPrice(obj, i) {
+  const cart = getCart();
+  const teddy = obj.find((item) => item._id == cart[i]);
   return teddy.price;
 }
 
-let total;
-
 function findTotal(arg) {
-  sum = 0;
-  for (i = 0; i < cart.length; i++) {
-    findPrice(arg);
-    sum = sum + teddy.price;
+  const cart = getCart();
+  let sum = 0;
+  for (let i = 0, j = cart.length; i < j; i++) {
+    const teddyPrice = findPrice(arg, i);
+    sum = sum + teddyPrice;
   }
-  return total = sum / 100;
+  return sum / 100;
 }
 
-function displayTotal() {
+function displayTotal(arg) {
+  const total = findTotal(arg);
   document.getElementById("total-price").textContent = total + ' ' + '\u20AC';
+  localStorage.setItem("total", JSON.stringify(total));
 }
 
 
@@ -81,28 +72,26 @@ function displayTotal() {
 
 function getObject() {
   fetch("http://localhost:3000/api/teddies")
-    .then(function (result) {
-      if (result.ok) {
-        return result.json();
-      }
-    })
-    .then(function (data) {
-        getCart();
-        findAllNames(data);
-        displayCart();
-        findTotal(data);
-        displayTotal();
-        return;
-    })
-    .catch(function (error) {
-        console.log("Une erreur s'est produite");
-    });
+  .then(function (result) {
+    if (result.ok) {
+      return result.json();
+    }
+  })
+  .then(function (data) {
+    displayCart(data);
+    displayTotal(data);
+  })
+  .catch(function (error) {
+console.log("Une erreur s'est produite");
+  });
 }
 
 getObject();
 
 
-// -- contact form functions
+
+
+// -- submit order functions
 
 const contact = {
   firstName: "",
@@ -111,6 +100,8 @@ const contact = {
   city: "",
   email: ""
 };
+
+const products = getCart();
 
 
 // security on submit button
@@ -132,6 +123,7 @@ function checkForm() {
   const c = document.getElementById("city").checkValidity();
   const a = document.getElementById("address").checkValidity();
   const e = document.getElementById("email").checkValidity();
+  let formIsValid;
   if (!fn || !ln || !a || !c || !e) {
     formIsValid = false;
     disableSubmit(true);
@@ -139,8 +131,10 @@ function checkForm() {
     formIsValid = true;
     disableSubmit(false);
   }
+  return formIsValid;
 }
 
+checkForm();
 
 document.getElementById("firstname").addEventListener("change", function (e) {
   contact.firstName = e.target.value;
@@ -166,7 +160,7 @@ document.getElementById("city").addEventListener("change", function (e) {
 // check email data
 
 document.getElementById("email").addEventListener("change", function (e) {
-  regex = /[a-zA-Z0-9][a-zA-Z0-9_\-\.]*@[a-zA-Z0-9][a-zA-Z0-9_\-\.]*\.[a-zA-Z]{2,5}/;
+  const regex = /[a-zA-Z0-9][a-zA-Z0-9_\-\.]*@[a-zA-Z0-9][a-zA-Z0-9_\-\.]*\.[a-zA-Z]{2,5}/;
   if (regex.test(e.target.value)) {
     contact.email = e.target.value;
   } else {
@@ -178,42 +172,51 @@ document.getElementById("email").addEventListener("change", function (e) {
 
 // assign cart value to products array
 
-function getProductsArray() {
-  getCart();
+/* function getProductsArray() {
+  const cart = getCart();
   products = cart;
   return products;
-}
+} */
 
 function checkProductsArray() {
-  getProductsArray();
+  const products = getCart();
+  console.log(products);
+  let arrayIsValid;
   if (Array.isArray(products)) {
     arrayIsValid = true;
+    console.log(arrayIsValid);
   } else {
     arrayIsValid = false;
+    console.log(arrayIsValid);
   }
+  return arrayIsValid;
 }
 
 
 // check order validity
 
 function checkOrder() {
+  const formIsValid = checkForm();
+  const arrayIsValid = checkProductsArray();
+  console.log(formIsValid);
+  console.log(arrayIsValid);
+  let orderIsValid;
   if (!formIsValid || !arrayIsValid) {
     alert("Order has failed, please check out your informations");
     orderIsValid = false;
   } else {
     orderIsValid = true;
   }
+  return orderIsValid;
 }
 
 
 // store order id and total price for confirmation page
 
-function storeOrder(arg) {
+function storeOrderId(arg) {
   const orderId = arg.orderId;
   localStorage.setItem("orderId", orderId);
-  localStorage.setItem("total", JSON.stringify(total));
 }
-
 
 // API request to send the contact object
 
@@ -233,8 +236,8 @@ function sendContact() {
   })
   .then(function (value) {
     console.log(value);
-    console.log(value.orderId);
-    storeOrder(value);
+    //console.log(value.orderId);
+    storeOrderId(value);
     location.href="confirmation.html";
   })
   .catch(function (error) {
@@ -244,9 +247,8 @@ function sendContact() {
 
 document.getElementById("submit").addEventListener("click", function (e) {
   e.preventDefault();
-  checkForm();
-  checkProductsArray();
-  checkOrder();
+  const orderIsValid = checkOrder();
+  console.log(orderIsValid);
   if (orderIsValid == true) {
     sendContact();
   }
